a54897b3a39a5593b1763ba2fdd112097087b874
Matt Teichman <teichman@uchicago.edu>
2026-06-10 13:35:09 -0500

    eliminate messing with the solver; keep the lack of timeout

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -1,12 +1,11 @@
 class Attc < Formula
   desc "Tool for batch-converting attachments to preservation formats"
   homepage "https://github.com/uchicago-library/attachment-converter"
-  url "https://github.com/uchicago-library/attachment-converter/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "262bfd77f51a8d45388701913f0acf2463820f5b66f8294d1dffc3a0a061821a"
+  url "https://github.com/uchicago-library/attachment-converter/archive/refs/tags/v0.1.9.tar.gz"
+  sha256 "f2c1c80ee16387b066e2ad204f7933a6033a5e1dfaeb038fd6f0905d78286b94"
   license "GPL-3.0-or-later"
 
   depends_on "opam" => :build
-  depends_on "aspcud" => :build
   depends_on "ghostscript"
   depends_on "mercurial"
   depends_on "pandoc"
@@ -15,7 +14,7 @@ class Attc < Formula
 
   def install
     ENV.deparallelize
-    system "env", "OPAMSOLVERTIMEOUT=0", "make", "pkg-build", "SOLVER=builtin-0install"
+    system "env", "OPAMSOLVERTIMEOUT=0", "make", "pkg-build"
     bin.install "_build/default/main.exe" => "attc"
     lib.install Dir["conversion-scripts/*"]
   end

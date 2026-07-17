0515248a64f694a83c87fe6d693163b64e0b9c68
Matt Teichman <teichman@uchicago.edu>
2026-07-17 16:44:01 -0500

    oops, forgot to update the checksum

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -1,7 +1,7 @@
 class Attc < Formula
   desc "Tool for batch-converting attachments to preservation formats"
   homepage "https://github.com/uchicago-library/attachment-converter"
-  url "https://dldc.lib.uchicago.edu/open/dist/attc/attachment-converter_0.2.10-4.tar.gz"
+  url "54b3392744f7162b4c2165b6af5aa0a50fd2491adb2b3580fb9c8addc48de3be"
   sha256 "b64e982b43e98279913b6e7274c82c792a7cdd9fb621d04e7c8c7a3056550f8d"
   license "GPL-3.0-or-later"
 

d79d7cdb0297161105381408389348b43ed8ca3f
Matt Teichman <teichman@uchicago.edu>
2026-07-20 11:48:20 -0500

    trying a binary formula

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -1,7 +1,7 @@
 class Attc < Formula
   desc "Tool for batch-converting attachments to preservation formats"
   homepage "https://github.com/uchicago-library/attachment-converter"
-  url "https://dldc.lib.uchicago.edu/open/dist/attc/attachment-converter_0.2.10-4.tar.gz"
+  url "https://dldc.lib.uchicago.edu/open/dist/attc/attc-macos_0.2.10-4.tar.gz"
   sha256 "54b3392744f7162b4c2165b6af5aa0a50fd2491adb2b3580fb9c8addc48de3be"
   license "GPL-3.0-or-later"
 
@@ -15,8 +15,9 @@ class Attc < Formula
 
   def install
     ENV.deparallelize
-    system "env", "OPAMSOLVERTIMEOUT=0", "make", "pkg-build", "gen-man-page"
-    bin.install "_build/default/main.exe" => "attc"
+    bin.install "attc"
+    # system "env", "OPAMSOLVERTIMEOUT=0", "make", "pkg-build", "gen-man-page"
+    # bin.install "_build/default/main.exe" => "attc"
     lib.install Dir["conversion-scripts/*"]
     man1.install "doc/attc.1"
   end

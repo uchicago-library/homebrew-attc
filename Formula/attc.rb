59eeeb5dbee631244e3c2f40dffea68c527bc36a
Matt Teichman <teichman@uchicago.edu>
2026-07-23 13:01:15 -0500

    try a libexec install

--- Formula/attc.rb
+++ Formula/attc.rb
@@ -15,7 +15,7 @@ class Attc < Formula
   def install
     ENV.deparallelize
     bin.install "attc"
-    bin.install Dir["conversion-scripts/*"]
+    libexec.install Dir["conversion-scripts/*"]
     man1.install "doc/attc.1"
   end
 
